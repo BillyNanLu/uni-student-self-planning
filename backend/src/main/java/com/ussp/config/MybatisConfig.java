3d95031f8cb2dd1f8548e018d37c83c565388f90
BillyNan_Lu <lunan96789@gmail.com>
2026-01-11 12:55:37 +0800

    feat(backend:config): add MybatisConfig to configure the MyBatis paging plugin PageInterceptor

---
+++ backend/src/main/java/com/ussp/config/MybatisConfig.java
@@ -0,0 +1,22 @@
+package com.ussp.config;
+
+import com.github.pagehelper.PageInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        PageInterceptor interceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        properties.setProperty("helperDialect", "mysql");
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        interceptor.setProperties(properties);
+        return interceptor;
+    }
+}
