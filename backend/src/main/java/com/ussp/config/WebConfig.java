17c81a89d84fb686c474c17c5c86d7648b2e7e0b
BillyNan_Lu <lunan96789@gmail.com>
2026-02-02 23:37:56 +0800

    fix(webconfig): amend some interceptors' configurations

--- backend/src/main/java/com/ussp/config/WebConfig.java
+++ backend/src/main/java/com/ussp/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ussp.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,6 +16,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 拦截器
-        registry.addInterceptor(loginInterceptor).excludePathPatterns("/users/login", "/users/register");
+        registry.addInterceptor(loginInterceptor)
+                .excludePathPatterns(
+                        "/users/login",
+                        "/users/register",
+                        "/forgotPassword/send-captcha",
+                        "/forgotPassword/reset-password",
+                        "/uploads/**"
+                );
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
     }
 }
