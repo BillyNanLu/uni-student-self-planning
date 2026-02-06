c4c30415bddbacd3623288dbb13de9968e75ae0d
BillyNan_Lu <lunan96789@gmail.com>
2026-02-06 22:27:25 +0800

    fix(backend:user-profile-tags): add three types of Map containers for evaluation

--- backend/src/main/java/com/ussp/pojo/OptionItem.java
+++ backend/src/main/java/com/ussp/pojo/OptionItem.java
@@ -3,6 +3,7 @@ package com.ussp.pojo;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class OptionItem {
@@ -10,6 +11,15 @@ public class OptionItem {
     private String label;      // 显示内容
     private Weights weights;   // 分数对象
     private List<String> tags;       // 标签
+
+    // 兴趣测评（RIASEC 六维度）
+    private Map<String, Integer> interest;
+
+    // 能力测评（五维度）
+    private Map<String, Integer> ability;
+
+    // 自我评价（）
+    private Map<String, Integer> selfEvaluation;
 
     @Data
     public static class Weights {
