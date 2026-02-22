b08b7a8d0d529cba1e07ee79733571efd534165a
BillyNan_Lu <lunan96789@gmail.com>
2026-02-22 23:58:58 +0800

    fix(backend:direction): add two fields about AI direction

--- backend/src/main/java/com/ussp/pojo/UserDirection.java
+++ backend/src/main/java/com/ussp/pojo/UserDirection.java
@@ -17,6 +17,9 @@ public class UserDirection {
     private Integer isConflict;         // 是否不一致（0=一致，1=不一致）
     private Integer confirmStatus;      // 用户是否确认系统建议（0未确认、1采纳系统、2拒绝系统）
 
+    private String aiReason;           // AI推荐方向理由
+    private String rejectReason;       // 用户拒绝系统建议的原因
+
     private LocalDateTime createTime;   // 创建时间
     private LocalDateTime updateTime;   // 更新时间
 }
