d7bacc1b4d0d2c249dda7dcd0478779825df436e
BillyNan_Lu <lunan96789@gmail.com>
2026-02-25 21:39:06 +0800

    feat(backend:score): add RIASECScore vo

---
+++ backend/src/main/java/com/ussp/vo/RIASECScoreVO.java
@@ -0,0 +1,18 @@
+package com.ussp.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RIASECScoreVO {
+    private int R;
+    private int I;
+    private int A;
+    private int S;
+    private int E;
+    private int C;
+}
+
