cdd7a5cc3d91620cc34c98d26cd12f30efac9d6c
BillyNan_Lu <lunan96789@gmail.com>
2026-05-12 21:57:11 +0800

    feat(backend:question): add administrator question management service

--- backend/src/main/java/com/ussp/service/QuestionService.java
+++ backend/src/main/java/com/ussp/service/QuestionService.java
@@ -6,4 +6,18 @@ import java.util.List;
 
 public interface QuestionService {
     List<Question> getQuestionsByQuestionnaireId(Long questionnaireId);
+
+    // 管理员新增题目
+    Question addQuestion(Question question);
+
+    // 管理员修改题目
+    boolean updateQuestion(Question question);
+
+    // 管理员删除单个问题
+    boolean deleteQuestion(Long id);
+
+    // 管理员批量删除问题
+    boolean batchDeleteQuestions(List<Long> ids);
+
+    List<Question> paginationQuery(Integer pageNum, Integer pageSize, Integer questionnaireId);
 }
