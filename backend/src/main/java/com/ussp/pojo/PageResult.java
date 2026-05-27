95814133d542f0c16e2fa0230361540a0630cf4d
BillyNan_Lu <lunan96789@gmail.com>
2026-05-27 23:52:12 +0800

    feat(backend:page): add page result pojo

---
+++ backend/src/main/java/com/ussp/pojo/PageResult.java
@@ -0,0 +1,15 @@
+package com.ussp.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult<T> {
+    private List<T> records;
+    private Integer total;
+}
