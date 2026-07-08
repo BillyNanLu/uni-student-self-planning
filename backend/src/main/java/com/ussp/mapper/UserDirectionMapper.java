ca7ad28f64fcb8e5399a80e9a660512c461955dc
BillyNan_Lu <lunan96789@gmail.com>
2026-07-08 23:50:18 +0800

    feat(backend:plan): add user direction mapper

--- backend/src/main/java/com/ussp/mapper/UserDirectionMapper.java
+++ backend/src/main/java/com/ussp/mapper/UserDirectionMapper.java
@@ -1,22 +1,35 @@
 package com.ussp.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import com.ussp.pojo.UserDirection;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserDirectionMapper {
 
+    @Select("SELECT COUNT(*) FROM user_direction WHERE final_direction = #{direction}")
+    Long countUserByFinalDirection(String direction);
+
     @Insert("""
-    INSERT INTO user_direction(user_id,preferred_direction,system_direction,is_conflict)
-    VALUES(#{userId},#{preferred},#{system},#{conflict})
+    INSERT INTO user_direction(user_id,preferred_direction)
+    VALUES(#{userId},#{preferred})
     ON DUPLICATE KEY UPDATE
-        preferred_direction=#{preferred},
-        system_direction=#{system},
-        is_conflict=#{conflict}
+        preferred_direction=#{preferred}
     """)
     void insertOrUpdate(@Param("userId") Long userId,
-                        @Param("preferred") String preferred,
-                        @Param("system") String system,
-                        @Param("conflict") int conflict);
+                        @Param("preferred") String preferred);
+
+    @Select("SELECT * FROM user_direction WHERE user_id=#{userId} ORDER BY create_time DESC LIMIT 1")
+    UserDirection findByUserId(Long userId);
+
+    @Update("UPDATE user_direction SET confirm_status=#{status}, final_direction=#{finalDirection}, reject_reason=#{rejectReason} WHERE user_id=#{userId} ORDER BY create_time DESC LIMIT 1")
+    void updateDirection(@Param("userId") Long userId,
+                         @Param("status") Integer status,
+                         @Param("finalDirection") String finalDirection,
+                         @Param("rejectReason") String rejectReason);
+
+    @Update("UPDATE user_direction SET final_direction=#{preferred} WHERE user_id=#{userId}")
+    void updateFinalDirection(Long userId, String preferred);
+
+    @Update("UPDATE user_direction SET ai_reason=#{aiReason}, system_direction=#{systemDirection}, is_conflict=#{isConflict}, final_direction=#{finalDirection} WHERE id = #{id}")
+    void updateAiReasonSystemDirectionAndISConflict(UserDirection ud);
 }
